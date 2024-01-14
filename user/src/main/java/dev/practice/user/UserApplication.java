f6d818e3ae2ba8e940ae1e8daad4c7b2e065db72
starryeye <see2818@naver.com>
2024-01-15 02:32:22 +0900

    [starryeye, 2024.01.15] - new article alert notification usecase 개발
    내용
    - kafka consumer & producer
    - article 에서 message 발행시 해당 message 를 구독하여 새로운 message 발행

--- user/src/main/java/dev/practice/user/UserApplication.java
+++ user/src/main/java/dev/practice/user/UserApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UserApplication {
 
+	// todo, sns-mysql 과 합쳐보기..
 	public static void main(String[] args) {
 		SpringApplication.run(UserApplication.class, args);
 	}
--- user/src/main/java/dev/practice/user/event/CreatedArticleEvent.java
+++ user/src/main/java/dev/practice/user/event/CreatedArticleEvent.java
@@ -1,5 +1,7 @@
 package dev.practice.user.event;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -9,8 +11,19 @@ public class CreatedArticleEvent {
     private final String articleId;
     private final String creatorUserId;
 
+    /**
+     * @JsonCreator, @JsonProperty ...
+     * ObjectMapper 로 String 을 Object 로 역직렬화 할때 no argument constructor 가 필요한데..
+     * immutable 객체에선 제공해주기가 불가능하므로.. 해당 annotation 을 사용하여 역직렬화 생성자로 만들어줌
+     *
+     * todo, record type 을 사용하면 어떨까?
+     */
+    @JsonCreator
     @Builder
-    private CreatedArticleEvent(String articleId, String creatorUserId) {
+    private CreatedArticleEvent(
+            @JsonProperty("articleId") String articleId,
+            @JsonProperty("creatorUserId") String creatorUserId
+    ) {
         this.articleId = articleId;
         this.creatorUserId = creatorUserId;
     }
