ca85103ebfd7d7119753d7ecf2ed8b146ce8226f
starryeye <see2818@naver.com>
2024-02-29 22:29:37 +0900

    [starryeye, 2024.02.29] - Follow Service 개발, R2dbc
    내용
    - Follow Service 개발, R2dbc

--- user/src/main/java/dev/practice/user/repository/FollowEntity.java
+++ user/src/main/java/dev/practice/user/repository/FollowEntity.java
@@ -7,10 +7,10 @@ import org.springframework.data.relational.core.mapping.Table;
 
 @Getter
 @Table("FOLLOW")
-public class FollowEntity extends BaseEntity{
+public class FollowEntity extends BaseEntity{ // auditing
 
     @Id
-    private final Long id;
+    private final Long id; // auto generate
 
     private final Long fromUserId;
     private final Long toUserId;
@@ -25,6 +25,7 @@ public class FollowEntity extends BaseEntity{
     // static 편의
     public static FollowEntity create(Long fromUserId, Long toUserId) {
         return FollowEntity.builder()
+                .id(null)
                 .fromUserId(fromUserId)
                 .toUserId(toUserId)
                 .build();
