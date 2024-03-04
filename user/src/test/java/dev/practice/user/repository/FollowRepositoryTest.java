6f2696866f9e19e9d963e9a4605049a2d6d6ed77
starryeye <see2818@naver.com>
2024-03-04 22:30:18 +0900

    [starryeye, 2024.03.04] - Follow Service 개발, R2dbc
    내용
    - Follow Service 개발, R2dbc

--- user/src/test/java/dev/practice/user/repository/FollowRepositoryTest.java
+++ user/src/test/java/dev/practice/user/repository/FollowRepositoryTest.java
@@ -8,8 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -37,7 +40,7 @@ class FollowRepositoryTest {
         assertThat(r2dbcEntityTemplate).isNotNull();
     }
 
-    @DisplayName("save 동작 테스트")
+    @DisplayName("save 정상 동작")
     @Test
     void save() {
 
@@ -68,6 +71,47 @@ class FollowRepositoryTest {
 
                             assertThat(follow.getFromUserId()).isEqualTo(1L);
                             assertThat(follow.getToUserId()).isEqualTo(2L);
+                        }
+                )
+                .verifyComplete();
+    }
+
+    @DisplayName("findByToUserId 메서드 쿼리 정상 동작")
+    @Test
+    void findByToUserId() {
+
+        // given
+        Long followerId = 20L;
+        FollowEntity followEntity1 = FollowEntity.create(10L, followerId);
+        FollowEntity followEntity2 = FollowEntity.create(11L, followerId);
+        FollowEntity followEntity3 = FollowEntity.create(12L, followerId);
+
+        Flux<FollowEntity> saved = followRepository.saveAll(List.of(followEntity1, followEntity2, followEntity3));
+
+        // when
+        Flux<FollowEntity> result = saved
+                .thenMany(
+                        followRepository.findByToUserId(followerId)
+                );
+
+        // then
+        StepVerifier.create(result)
+                .assertNext(
+                        followEntity -> {
+                            assertThat(followEntity.getFromUserId()).isEqualTo(10L);
+                            assertThat(followEntity.getToUserId()).isEqualTo(followerId);
+                        }
+                )
+                .assertNext(
+                        followEntity -> {
+                            assertThat(followEntity.getFromUserId()).isEqualTo(11L);
+                            assertThat(followEntity.getToUserId()).isEqualTo(followerId);
+                        }
+                )
+                .assertNext(
+                        followEntity -> {
+                            assertThat(followEntity.getFromUserId()).isEqualTo(12L);
+                            assertThat(followEntity.getToUserId()).isEqualTo(followerId);
                         }
                 )
                 .verifyComplete();
